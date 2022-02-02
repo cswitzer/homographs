9de22d6363dd3b43ec22f87375d65fbd5364bb1c
chaseswizzleswiz@gmail.com <chaseswizzleswiz@gmail.com>
2022-02-02 13:54:12 -0700

    added question

--- week05/canon.py
+++ week05/canon.py
@@ -2,16 +2,25 @@
 # 2. find both files at the file paths based on the strings passed in
 # 3. if files are the same, it's a homograph
 
+from fileinput import filename
+
+
 filename_one = input("Specify the first filename: ")
 filename_two = input("Specify the second filename: ")
 
-pwd = "C:\\Users\\Chase Switzer\\source\\Python\\Practice\\week05\\canon.py"
+pwd = "C:\\Users\\Chase Switzer\\source\\Python\\Practice\\"
 
 def canon(filename_one, filename_two):
     pwdArray = pwd.split('\\')
-    print(pwdArray)
     #test.txt
-    #../../cs453/week05/test.txt
+    #..\..\Python\Practice\test.txt
+
+    fileOneArray = filename_one.split('\\')
+    fileTwoArray = filename_two.split('\\')
+    
+    
+    
+
     pass
 
 canon(filename_one, filename_two)
