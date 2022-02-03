e369ff1b59e491409dc1ee4def724a1a66e3e4ac
chaseswizzleswiz@gmail.com <chaseswizzleswiz@gmail.com>
2022-02-03 13:04:10 -0700

    fixed some bugs

--- week05/canon.py
+++ week05/canon.py
@@ -8,19 +8,33 @@ from fileinput import filename
 filename_one = input("Specify the first filename: ")
 filename_two = input("Specify the second filename: ")
 
-pwd = "C:\\Users\\Chase Switzer\\source\\Python\\Practice\\"
+pwd = "C:\\Users\\Chase Switzer\\source\\Python\\Practice\\week05"
 
 def canon(filename_one, filename_two):
     pwdArray = pwd.split('\\')
+
     #test.txt
     #..\..\Python\Practice\test.txt
 
     fileOneArray = filename_one.split('\\')
     fileTwoArray = filename_two.split('\\')
-    
-    
-    
 
-    pass
+    print(fileTwoArray)
+
+    biggest_array_size = 0
+
+    if len(fileOneArray) >= len(fileTwoArray):
+        biggest_array_size = len(fileOneArray)
+    else:
+        biggest_array_size = len(fileTwoArray)
+
+    # if filenames aren't the same, they are not homographs
+    if fileOneArray[-1] != fileTwoArray[-1]:
+            return False
+
+    # check for ./
+    for i in range(len(fileOneArray)):
+        print(i)
+    # check for ../
 
 canon(filename_one, filename_two)
